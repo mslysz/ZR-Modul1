b0ec44bd37e53e1ca5184d7f7c66004fd9ce0c92
mslysz <macslysz@gmail.com>
2024-10-21 01:19:16 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { LikesCounter } from './components/LikesCounter';
+import { Plot } from './components/Plot';
+
+function App() {
+  return (
+    <>
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <LikesCounter />
+      <Plot />
+    </>
+  );
+}
+
+export default App;
