8bd373d185fd92cc82c9615da6f56a7392d2377b
mslysz <macslysz@gmail.com>
2024-10-21 02:47:58 +0100

    added: state for input and for textarea, when the form is submitted, a review object with author and text data is created and added as the current value of the review state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,68 @@
+import { useState } from 'react';
+
 export const Form = () => {
+  const [review, setReview] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [textareaValue, setTextareaValue] = useState('');
+
+  console.log(review);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const author = inputValue;
+    const text = textareaValue;
+
+    setReview({ author, text });
+  }
+
+  console.log('Wartość zmiennej stanowej input:', inputValue);
+  console.log('Wartość zmiennej stanowej textarea:', textareaValue);
+
   return (
     <>
+      {review && (
+        <article>
+          <strong>{review.author}</strong>
+          <p>{review.text}</p>
+        </article>
+      )}
       <h2>Dodaj recenzję</h2>
-      <form action=''>
+      <form onSubmit={handleSubmit}>
         <div>
           <div>
             <label htmlFor='author'>Autor</label>
           </div>
-          <input type='text' name='author' id='author' />
+          <input
+            type='text'
+            name='author'
+            id='author'
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
         </div>
         <div>
           <div>
             <label htmlFor='text'>Tekst</label>
           </div>
-          <textarea name='text' id='text'></textarea>
+          <textarea
+            name='text'
+            id='text'
+            value={textareaValue}
+            onChange={(e) => {
+              setTextareaValue(e.target.value);
+            }}
+          ></textarea>
         </div>
-        <button type='submit'>Dodaj</button>
+        <button
+          type='submit'
+          //the form can only be sent when the input and textarea fields are filled in
+          disabled={inputValue === '' || textareaValue === ''}
+        >
+          Dodaj
+        </button>
       </form>
     </>
   );
